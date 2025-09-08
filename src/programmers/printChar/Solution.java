97f4cb6200b5a4737d5e26507b3680d57206d81e
nawonnnnnn <__980202@hanmail.net>
2025-09-08 18:41:57 +0900

    문자 출력 / Lv0

---
+++ src/programmers/printChar/Solution.java
@@ -0,0 +1,16 @@
+package programmers.lv0.printChar;
+
+// 출력예시
+//3
+//2
+//1
+//Let's go!
+
+public class Solution {
+    public static void main(String[] args) {
+        String message = "Let's go!";
+
+        System.out.println("3\n 2\n 1");
+        System.out.println(message);
+    }
+}
