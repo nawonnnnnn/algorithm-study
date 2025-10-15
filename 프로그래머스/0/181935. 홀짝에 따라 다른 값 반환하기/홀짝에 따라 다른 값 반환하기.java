ad2c8c17f44bb7980793f68dc3bf4e9af670af0a
LeeNawon <81615776+nawonnnnnn@users.noreply.github.com>
2025-10-16 00:00:57 +0900

    Update 홀짝에 따라 다른 값 반환하기.java

--- 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.java
+++ 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.java
@@ -1,21 +1,15 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        
-        if(n % 2 == 0) {
-            for(int i = 0; i <= n; i++){
-                if(i % 2 == 0){
-                    answer += i * i;
-                }
+        if (n % 2 == 1) {
+            for (int i=1; i<=n; i+=2) {
+                answer += i;
             }
         } else {
-            for(int j = 0; j <= n; j++){
-                if(j % 2 != 0){
-                    answer += j;
-                }
-            }            
+            for (int i=2; i<=n; i+=2) {
+                answer += i*i;
+            }
         }
-        
         return answer;
     }
 }
