713bc6063459a6d1dacb05ffabdfb8cb790b61b8
LeeNawon <81615776+nawonnnnnn@users.noreply.github.com>
2025-10-15 23:31:47 +0900

    [level 0] Title: 더 크게 합치기, Time: 1.79 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int a, int b) {
+        
+        int answer = 0;
+        
+        String ab = String.valueOf(a) + String.valueOf(b);
+        String ba = String.valueOf(b) + String.valueOf(a);
+        
+        int abValue = Integer.parseInt(ab);
+        int baValue = Integer.parseInt(ba);
+        
+        answer = abValue >= baValue ? abValue : baValue;      
+        
+        return answer;
+    }
+}
