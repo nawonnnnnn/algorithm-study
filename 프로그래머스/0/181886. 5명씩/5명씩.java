2430495fecbdcc6dffd35563ab93a95b70a16024
nawonnnnnn <81615776+nawonnnnnn@users.noreply.github.com>
2026-06-03 18:21:52 +0900

    [level 0] Title: 5명씩, Time: 0.02 ms, Memory: 66.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181886. 5명씩/5명씩.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String[] solution(String[] names) {
+
+        int size = (names.length + 4) / 5;
+        String[] answer = new String[size];
+
+        int idx = 0;
+
+        for(int i = 0; i < names.length; i += 5) {
+            answer[idx++] = names[i];
+        }
+
+        return answer;
+    }
+}
