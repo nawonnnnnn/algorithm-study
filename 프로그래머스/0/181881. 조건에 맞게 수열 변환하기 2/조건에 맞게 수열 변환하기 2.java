547642a12b1f56942918415e55ece706fdbbc974
nawonnnnnn <81615776+nawonnnnnn@users.noreply.github.com>
2026-06-06 19:35:52 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 2, Time: 48.71 ms, Memory: 114 MB -BaekjoonHub

--- 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.java
+++ 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.java
@@ -5,10 +5,9 @@ class Solution {
         int answer = 0;
         
         while (true) {
-            // 이전 상태의 배열을 복사
+
             int[] prevArr = arr.clone(); 
             
-            // 조건에 맞게 배열의 원소 변환
             for (int i = 0; i < arr.length; i++) {
                 if (arr[i] >= 50 && arr[i] % 2 == 0) {
                     arr[i] /= 2;
@@ -17,7 +16,6 @@ class Solution {
                 }
             }
             
-            // 변환 전후 배열 비교
             if (Arrays.equals(prevArr, arr)) {
                 break;
             }
